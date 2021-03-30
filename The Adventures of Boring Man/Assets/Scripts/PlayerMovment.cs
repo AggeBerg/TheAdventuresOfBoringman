d3242014a94e8630bb174da9fb1027094afc946a
Agathon Bergroth <agathon.bergroth@abbindustrigymnasium.se>
2021-03-30 16:21:09 +0200

    Added player
    Can move and look at cursor

---
+++ The Adventures of Boring Man/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public Rigidbody2D Body;
+    public float speed;
+    public Transform transform;
+    
+    void start()
+    {
+        // use this function to initialize
+        // anything
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Body.velocity = (new Vector2(Input.GetAxisRaw("Horizontal")*speed, Input.GetAxisRaw("Vertical")*speed));
+    }
+
+}
---
+++ The Adventures of Boring Man/Assets/Scripts/RotateToCursor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToCursor : MonoBehaviour
+{
+ public Vector3 mouse_pos;
+ public Transform target; //Assign to the object you want to rotate
+ public Vector3 object_pos;
+ public float angle;
+ 
+ void Update () {
+         //rotation
+         Vector3 mousePos = Input.mousePosition;
+         mousePos.z = 5.23f;
+ 
+         Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+         mousePos.x = mousePos.x - objectPos.x;
+         mousePos.y = mousePos.y - objectPos.y;
+ 
+         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg + 3;
+         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+     }
+ }
