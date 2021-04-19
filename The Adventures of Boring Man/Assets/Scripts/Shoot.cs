fb0652a9fd4d14b1246ad371b095fbe53c90880b
Agathon Bergroth <agathon.bergroth@abbindustrigymnasium.se>
2021-04-19 16:33:48 +0200

    Shooting
    Added Shooting

---
+++ The Adventures of Boring Man/Assets/Scripts/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform Muzzle;
+    public int Layer;
+    public SpriteRenderer Sprite;
+    public float Flashtime;
+    private float TimeSinceLastShoot;
+    private float LastShoot;
+
+    void Start(){
+        
+        Sprite.color = new Color(1f, 1f ,1f , 0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        TimeSinceLastShoot = Time.time - LastShoot;
+        if (Input.GetMouseButtonDown(0)){
+            Debug.Log("First thing today");
+            Physics2D.Raycast(Muzzle.position, new Vector2(Muzzle.rotation.x, Muzzle.rotation.y), 500, Layer);
+            Sprite.color = new Color(1f, 1f ,1f , 1f);
+            LastShoot = Time.time;
+        } else if (TimeSinceLastShoot > Flashtime) {
+            Sprite.color = new Color(1f, 1f ,1f , 0f);
+        }
+    }
+}
